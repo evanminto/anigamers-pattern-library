a1e43d63285b1c5962dc87cb1a9fad0424ee23d3
Evan Minto <evan.minto@gmail.com>
2018-03-09 21:09:04 -0800

    Set up PostCSS

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const del = require('del');
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const sass = require('gulp-sass');
+
+const browsersList = ['>= 0.5%'];
+
+gulp.task('clean-css', () => {
+  return del([
+    'public/css/*',
+  ]);
+});
+
+gulp.task('build-css-dev', () => {
+  return gulp.src('src/scss/**')
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({ browsers: browsersList }),
+    ]))
+    .pipe(gulp.dest('public/css/'))
+});
+
+gulp.task('build-css-prod', () => {
+  return gulp.src('src/scss/**')
+    .pipe(sass())
+    .pipe(postcss([
+      autoprefixer({ browsers: browsersList }),
+      cssnano(),
+    ]))
+    .pipe(gulp.dest('public/css/'));
+});
+
+gulp.task('css-dev', ['clean-css', 'build-css-dev']);
+gulp.task('css-prod', ['clean-css', 'build-css-prod']);
+
+gulp.task('css-watch', () => {
+  return gulp.watch('src/scss/**', ['css-dev']);
+});
