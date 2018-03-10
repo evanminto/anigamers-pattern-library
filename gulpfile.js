708785ef63e7a24ed9d20b4cc7ff2a1090f2ccbd
Evan Minto <evan.minto@gmail.com>
2018-03-09 21:36:54 -0800

    Make CSSNano minification safe

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,9 @@ gulp.task('build-css-prod', () => {
     .pipe(sass())
     .pipe(postcss([
       autoprefixer({ browsers: browsersList }),
-      cssnano(),
+      cssnano({
+        safe: true,
+      }),
     ]))
     .pipe(gulp.dest('public/css/'));
 });
